820e6da1c04bce20473fd2ebc424b6ee520e748f
Sejin Kim <froydroyce@gmail.com>
2019-11-05 11:39:27 -0700

    Remove like forst matching have_many events

--- spec/models/sport_spec.rb
+++ spec/models/sport_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Sport do
   describe 'relations' do
-    it { should have_many :olympian_sports }
+    it { should have_many :events }
     it { should have_many :olympians }
   end
 end
