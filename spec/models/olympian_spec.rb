938b523383625b7f5ac30b238c800fb870547186
Sejin Kim <froydroyce@gmail.com>
2019-11-05 14:22:56 -0700

    Add tests for avg_weight and avg_age

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -18,16 +18,34 @@ RSpec.describe Olympian do
         name: "Sejin Kim",
         age: 31,
         sex: "M",
-        weight: 185,
+        weight: 60,
+        height: 60,
+        team: @korea,
+        sport: @taekwondo
+      )
+      @maria = Olympian.create!(
+        name: "Maria Kim",
+        age: 25,
+        sex: "F",
+        weight: 40,
         height: 60,
         team: @korea,
         sport: @taekwondo
       )
       @jaren = Olympian.create!(
         name: "Jaren Kim",
-        age: 36,
+        age: 4,
         sex: "M",
-        weight: 185,
+        weight: 23,
+        height: 60,
+        team: @usa,
+        sport: @boxing
+      )
+      @jamie = Olympian.create!(
+        name: "Jamie Kim",
+        age: 7,
+        sex: "F",
+        weight: 21,
         height: 60,
         team: @usa,
         sport: @boxing
@@ -35,8 +53,17 @@ RSpec.describe Olympian do
     end
 
     it ".by_age(age)" do
-      expect(Olympian.by_age("youngest").take).to eq(@sejin)
-      expect(Olympian.by_age("oldest").take).to eq(@jaren)
+      expect(Olympian.by_age("youngest")).to eq(@jaren)
+      expect(Olympian.by_age("oldest")).to eq(@sejin)
+    end
+
+    it ".avg_weight(sex)" do
+      expect(Olympian.avg_weight("M").to_f.round(1)).to eq(41.5)
+      expect(Olympian.avg_weight("F").to_f.round(1)).to eq(30.5)
+    end
+
+    it ".avg_age" do
+      expect(Olympian.avg_age.to_f.round(1)).to eq(16.8)
     end
   end
 end
