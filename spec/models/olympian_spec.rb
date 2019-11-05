2ec61cf397aabba22254ed7d938102cca2729a31
Sejin Kim <froydroyce@gmail.com>
2019-11-05 11:39:58 -0700

    Update test to work with removing table

--- spec/models/olympian_spec.rb
+++ spec/models/olympian_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe Olympian do
   describe 'relations' do
     it { should belong_to :team }
+    it { should belong_to :sport }
     it { should have_many :olympian_events }
-    it { should have_many :olympian_sports }
     it { should have_many :events }
-    it { should have_many :sports }
   end
 end
