ecb1e55a2b51e5c27a77d0dbc6015a9bc413a3bc
Sejin Kim <froydroyce@gmail.com>
2019-11-05 18:37:11 -0700

    Add show action and before action with facade to events controller

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::EventsController < ApplicationController
+  before_action :event_facade
+
   def index
     render json: EventSerializer.new(Sport.includes(:events))
+  end
+
+  def show
+    render json: EventMedalistSerializer.new(@facade)
+  end
+
+  private
+
+  def event_facade
+    @facade = EventFacade.new(params[:id])
   end
 end
